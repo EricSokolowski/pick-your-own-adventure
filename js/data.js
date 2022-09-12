9b64bedde838124c8e6b937b5ad9721075281a0a
EricSoklowski <ericsoko2002@gmail.com>
2022-09-11 21:52:49 -0400

    create and import data.js

---
+++ js/data.js
@@ -0,0 +1,19 @@
+const chapters = [
+  {
+    currentScene: 'start',
+    start: {
+      title: `CHapter 0`,
+      story: `Oi, stranger, you here for the bounty?  There's an Owlbear out in the woods thats been terrorizing us you see!  If ye give it a wack i'll get ye whatever drink ye wish!!!`,
+      options: [
+        {
+          option: `Venture out`
+        },
+        {
+          option: `Too Dangerous`
+        }
+      ]
+    }
+  }
+]
+
+export {chapters}
